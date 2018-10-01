e24e6fb01927e9367b5f76632af69938a4e2058d
Vishal Limbachiya <vishallimbachiya379@gmail.com>
2018-10-01 08:22:45 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+  angular.module('app', ['ngRoute'])
+    //---------------
+    // Services
+    //---------------
+    .factory('Todos', function(){
+      return [
+        { name: 'AngularJS Directives', completed: true, note: 'add notes...' },
+        { name: 'Data binding', completed: true, note: 'add notes...' },
+        { name: '$scope', completed: true, note: 'add notes...' },
+        { name: 'Controllers and Modules', completed: true, note: 'add notes...' },
+        { name: 'Templates and routes', completed: true, note: 'add notes...' },
+        { name: 'Filters and Services', completed: false, note: 'add notes...' },
+        { name: 'Get started with Node/ExpressJS', completed: false, note: 'add notes...' },
+        { name: 'Setup MongoDB database', completed: false, note: 'add notes...' },
+        { name: 'Be awesome!', completed: false, note: 'add notes...' },
+      ];
+    })
+    //---------------
+    // Controllers
+    //---------------
+    .controller('TodoController', ['$scope', 'Todos', function ($scope, Todos) {
+      $scope.todos = Todos;
+    }])
+    .controller('TodoDetailCtrl', ['$scope', '$routeParams', 'Todos', function ($scope, $routeParams, Todos) {
+      $scope.todo = Todos[$routeParams.id];
+    }])
+    //---------------
+    // Routes
+    //---------------
+    .config(['$routeProvider', function ($routeProvider) {
+      $routeProvider
+        .when('/', {
+          templateUrl: 'pages/todos.html',
+          controller: 'TodoController'
+        })
+        .when('/:id', {
+          templateUrl: 'pages/todoDetails.html',
+          controller: 'TodoDetailCtrl'
+       });
+    }]);
